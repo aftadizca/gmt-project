f3207858db6e3b939a045695227b44cbebcce8e0
Afta Dizca Wahana <aftadizca@gmail.com>
2019-04-09 02:29:20 +0700

    added mock chart

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,74 @@
 import React, { Component } from "react";
 import { Grid, Statistic } from "semantic-ui-react";
+import { Line } from "react-chartjs-2";
 
 class Home extends Component {
   state = {};
   render() {
+    const data = {
+      labels: [
+        "January",
+        "February",
+        "Maret",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ],
+      datasets: [
+        {
+          label: "MATERIAL INCOMING",
+          data: [10000, 50000, 6565, 55545, 8000, 3, 0, 0, 0, 0, 0, 0],
+          backgroundColor: ["rgba(33, 133, 208, 0.5)"],
+          borderColor: [
+            "rgba(0, 92, 240, 1)",
+            "rgba(0, 92, 240, 1)",
+            "rgba(0, 92, 240, 1)",
+            "rgba(0, 92, 240, 1)",
+            "rgba(0, 92, 240, 1)",
+            "rgba(0, 92, 240, 1)",
+            "rgba(0, 92, 240, 1)",
+            "rgba(0, 92, 240, 1)",
+            "rgba(0, 92, 240, 1)",
+            "rgba(0, 92, 240, 1)",
+            "rgba(0, 92, 240, 1)",
+            "rgba(0, 92, 240, 1)"
+          ],
+          borderWidth: 3
+        },
+        {
+          label: "MATERIAL OUTCOMING",
+          data: [7898, 5700, 56989, 55545, 8000, 3, 0, 0, 0, 0, 0, 0],
+          backgroundColor: ["rgba(255, 112, 112, 0.4)"],
+          borderColor: [
+            "rgba(255, 0, 0, 1)",
+            "rgba(255, 0, 0, 1)",
+            "rgba(255, 0, 0, 1)",
+            "rgba(255, 0, 0, 1)",
+            "rgba(255, 0, 0, 1)",
+            "rgba(255, 0, 0, 1)",
+            "rgba(255, 0, 0, 1)",
+            "rgba(255, 0, 0, 1)",
+            "rgba(255, 0, 0, 1)",
+            "rgba(255, 0, 0, 1)",
+            "rgba(255, 0, 0, 1)",
+            "rgba(255, 0, 0, 1)"
+          ],
+          borderWidth: 3
+        }
+      ]
+    };
+
     return (
-      <Grid columns={3} divided>
-        <Grid.Row />
+      <Grid>
+        <Grid.Row>
+          <Line data={data} />
+        </Grid.Row>
       </Grid>
     );
   }
