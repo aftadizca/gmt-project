f3f8487b67a5be305849dcc859c894227527d8f8
Afta Dizca Wahana <aftadizca@gmail.com>
2019-05-03 03:08:04 +0700

    cleaning code

--- src/_helper/tool.js
+++ src/_helper/tool.js
@@ -28,7 +28,6 @@ import _ from "lodash";
  */
 
 export function getByProperty(object, key, value, returnedKey) {
-  //console.log({ object, key, value, returnedKey });
   if (object.length === 0) {
     return;
   }
